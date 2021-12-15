a14c4b45e43184fac26cb522cfcdf864f69c52c6
Icefoxzhx <1104161801@qq.com>
2021-12-15 15:41:34 +0800

    final version

--- try.py
+++
@@ -1,5 +0,0 @@
-filename = 'A.txt'
-with open(filename, 'r') as f:
-	lines = f.readlines()
-	for line in lines:
-		print('cp -r ScanNet_with_eric_/{}_with_eric ScanNet_with_eric'.format(line.split()[1]))
--- utilize.sh
+++
@@ -1,4 +0,0 @@
-for dir in `ls ../ScanNet/scans`; do
-  # echo `mkdir -p ScanNet/scans_test/${dir}`
-  echo python make_data.py --scanID $dir --data_dir ..\\ScanNet\\scans --labels_dir ..\\ScanNet\\scannetv2-labels.combined.tsv
-done
